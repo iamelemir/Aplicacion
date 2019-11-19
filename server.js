db60bd6dad64fd2f85d30e599e835db09269bc05
iamelemir <elemir18@gmail.com>
2019-11-18 20:35:13 -0500

    server file

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+const http = require('http');
+const server = http.createServer(function (req, res) {
+console.log("Petición Recibida.");
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hola Mundo');
+});
+server.listen(8000);
+console.log("Servidor Iniciado.");
+
+
