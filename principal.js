d8dd95ac6000cb13d214c6c14a414634a35db603
iamelemir <elemir18@gmail.com>
2019-12-08 12:08:43 -0500

    new

--- principal.js
+++ principal.js
@@ -1,7 +1,39 @@
+
+/*
 var x = prompt("ingrese un numero");
 var y = prompt("ingrese un numero");
 num1 = parseInt(x);
 num2 = parseInt(y);
+  */
+
+ for (i=1;i<10;i++){
+    document.write("<br><b>La tabla del " + i + ":</b><br>")
+    for (j=1;j<10;j++) {
+    document.write(i + " x " + j + ": ")
+    document.write(i*j)
+    document.write("<br>")
+    }
+    }
+
+/*
+
+    var suma=0;
+
+       while (suma < 1000) {  
+        
+        suma += parseInt(Math.random() * 100);
+        document.write(suma + "<br>");
+    }
+
+
+/*
+    var i;
+   for(i=1; i<6; i++){
+   
+   document.write("<H" + i + "> encabezado nivel " + i + "</H" + i + ">" );
+   
+   }
+*/
 
 /*
   var z =  prompt("Ingrese 1 para SUMAR \n"
