fee7beb4b7681830c8ee68e58df468b22650dc00
iamelemir <elemir18@gmail.com>
2020-01-09 15:54:27 -0500

    new changes

---
+++ changes.js
@@ -0,0 +1,140 @@
+
+/*
+var x = prompt("ingrese un numero");
+var y = prompt("ingrese un numero");
+num1 = parseInt(x);
+num2 = parseInt(y);
+  */
+/***
+ * for (i=1;i<10;i++){
+    document.write("<br><b>La tabla del " + i + ":</b><br>")
+    for (j=1;j<10;j++) {
+    document.write(i + " x " + j + ": ")
+    document.write(i*j)
+    document.write("<br>")
+    }
+    }
+ *  function saludoInicial(){
+        var nombre = prompt("ingresa tu nombre");
+        document.write("<H1> Hola " + nombre +  ", como estas?</H1>");
+        
+    }
+ * 
+ */
+
+
+/**function media(valor1,valor2){
+    var resultado
+    resultado = (valor1 + valor2) / 2
+    return resultado
+    }
+    
+var media_date = media(20,30);
+document.write( media_date ,"<br/>");
+document.write(isNaN(media));
+ * 
+ * 
+ * 
+
+ */
+
+
+
+
+
+
+/**
+ * 
+ * git add .
+ * git commit
+ * git push
+ * 
+ */
+/*
+
+    var suma=0;
+
+       while (suma < 1000) {  
+        
+        suma += parseInt(Math.random() * 100);
+        document.write(suma + "<br>");
+    }
+
+
+/*
+    var i;
+   for(i=1; i<6; i++){
+   
+   document.write("<H" + i + "> encabezado nivel " + i + "</H" + i + ">" );
+   
+   }
+*/
+
+/*
+  var z =  prompt("Ingrese 1 para SUMAR \n"
+        +"Ingrese 2 para RESTAR \n"
+        +"Ingrese 3 para MULTIPLICAR \n"
+        +"Ingrese 4 para DIVIDIR \n");
+
+    var date = parseInt(z);
+
+            switch (date) {
+                
+                case 1:
+                        //var result = 
+                          alert("El resultado de la SUMA es : " + (num1+num2));  
+                    break;
+
+                case 2:
+                          alert("El resultado de la RESTA es : " + (num1-num2));
+                    break;
+
+                case 3:
+                          alert("El resultado de la MULTIPLICACION es : " + (num1*num2));
+                    break;
+                
+                case 4:
+                          alert("El resultado de la DIVISION es : " + (num1/num2));
+                    break;
+                default:
+                            alert("No esocogió opcion");
+                    break;
+            }
+*/
+
+/*
+        
+        var x = prompt("ingrese un numero");
+        var y = prompt("ingrese un numero");
+
+        var c = parseInt(x) + parseInt(y);
+         alert("la suma de los dos numeros es : "+c);
+
+         var fecha = Date();
+         
+         z = (x<y) ? 'es mayor': 'es menor' ;
+         alert(z);
+        // document.write(fecha);
+
+        
+         alert(fecha);
+         /**
+ * var num1 = 2;
+
+var num3 = 5;
+ */
+
+
+
+
+//alert(num1+num3);
+/**if(num1==2 && num3!=4){
+
+    alert(true);
+
+}else{
+
+    alert(false );
+}
+ * 
+ */
