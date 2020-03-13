fd9fe31076f1f0b99357f641c1d6245c0280250c
iamelemir <elemir18@gmail.com>
2020-03-13 13:13:50 -0500

    master ok

--- form/form.js
+++ form/form.js
@@ -20,3 +20,15 @@ function validacion() {
     }
 }
 window.addEventListener("load", iniciar, false); */
+
+
+function validacion() {
+    if (nombre1.value == '' && nombre2.value == '') {
+        nombre1.setCustomValidity('inserte al menos un nombre');
+
+        nombre1.style.background = '#FFDDDD';
+    } else {
+        nombre1.setCustomValidity('');
+        nombre1.style.background = '#FFFFFF';
+    }
+}
